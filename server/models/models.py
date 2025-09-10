6b6b4e171506e6563f8bc61b3846059796d2d394
MertcanSonat56 <mertcanyoldas7@gmail.com>
2025-09-11 00:42:04 +0300

    backend and frontend done

--- server/models/models.py
+++ server/models/models.py
@@ -16,3 +16,7 @@ class StoryNodeLLM(BaseModel):
 class StoryLLMResponse(BaseModel):
     title: str = Field(description="The title of the story")
     rootNode: StoryNodeLLM = Field(description="The root node of the story")
+    
+    
+    
+    
